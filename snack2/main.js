9bf626a080cece2a3b3c320494f18ed913c4dc2e
GIGIPR3 <gigiprencipe47@gmail.com>
2022-10-25 23:13:24 +0200

    esercizio 2

--- snack2/main.js
+++ snack2/main.js
@@ -3,8 +3,10 @@
 Generatore di “nomi cognomi” casuali del grande gatsby:
 Abbiamo una lista di nomi e una lista di cognomi, e da queste vogliamo generare una "falsa" lista di invitati, dove ogni invitato nella lista, è l'unione di un "nome" e un "cognome" delle liste precedentemente dette, scelti in modo casuale. */
 
-let numeroInvitati = parseInt(prompt("Qanti invitati vuoi alla festa?"));
-console.log(numeroInvitati);
+let risposta = parseInt(prompt("non ho capito cosa devo fare, dimmi si per andare avanti e no per non farlo"));
+console.log(risposta);
+
+const getRandomInt = ( min = 0, max = 1 );
 
 const listaCognomi = ["rossi", "verdi", "gialli", "blu"]
 
