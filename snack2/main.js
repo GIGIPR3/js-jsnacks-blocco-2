41b9b8d08e99f641579f4bad842234ad3c4d0eea
GIGIPR3 <gigiprencipe47@gmail.com>
2022-10-25 20:58:34 +0200

    .

---
+++ snack2/main.js
@@ -0,0 +1,5 @@
+/* 
+! Snack 2*
+Generatore di “nomi cognomi” casuali del grande gatsby:
+Abbiamo una lista di nomi e una lista di cognomi, e da queste vogliamo generare una "falsa" lista di invitati, dove ogni invitato nella lista, è l'unione di un "nome" e un "cognome" delle liste precedentemente dette, scelti in modo casuale. */
+
---
+++ snack3/main.js
@@ -0,0 +1,4 @@
+
+/* 
+! Snack 3 
+Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari */
