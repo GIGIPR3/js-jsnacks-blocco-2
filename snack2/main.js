a39166a2bd1947406558eee8790f4b43f00140c4
GIGIPR3 <gigiprencipe47@gmail.com>
2022-10-26 01:11:19 +0200

    .

--- snack2/main.js
+++ snack2/main.js
@@ -3,12 +3,23 @@
 Generatore di “nomi cognomi” casuali del grande gatsby:
 Abbiamo una lista di nomi e una lista di cognomi, e da queste vogliamo generare una "falsa" lista di invitati, dove ogni invitato nella lista, è l'unione di un "nome" e un "cognome" delle liste precedentemente dette, scelti in modo casuale. */
 
-let risposta = parseInt(prompt("non ho capito cosa devo fare, dimmi si per andare avanti e no per non farlo"));
+/* let risposta = parseInt(prompt("non ho capito cosa devo fare, dimmi si per andare avanti e no per non farlo"));
 console.log(risposta);
 
-const getRandomInt = ( min = 0, max = 1 );
+ */
 
-const listaCognomi = ["rossi", "verdi", "gialli", "blu"]
+var listaCognomi = ["rossi", "verdi", "gialli", "blu"];
 
-const listaNomi = ["luca", "marco", "giuseppe", "francesco"]
+
+var listaNomi = ["luca", "marco", "giuseppe", "francesco"];
+
+var rand = Math.floor(Math.random() * listaNomi.length);
+
+var rando = Math.floor(Math.random() * listaCognomi.length);
+
+let cogn = listaCognomi[rando]
+let nom = listaNomi [rand]
+console.log(nom + " " + cogn)
+
+
 
--- snack3/main.js
+++ snack3/main.js
@@ -2,3 +2,6 @@
 /* 
 ! Snack 3 
 Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari */
+
+const numeri = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
