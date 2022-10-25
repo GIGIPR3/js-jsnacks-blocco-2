85f3d140bba3fd56267cd64270c14cc97f818627
GIGIPR3 <gigiprencipe47@gmail.com>
2022-10-25 17:29:16 +0200

    esercizio 1 completato

---
+++ snack1/main.js
@@ -0,0 +1,10 @@
+/* 
+!Snack 1
+Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo ( del numero inserito dall'utente: esempio se il numero dell'utente è 2, il numero successivo è 3  ). */
+
+let numero = parseInt(prompt("scrivimi un numero"))
+console.log(numero)
+
+if ( (numero % 2) !=0 ) {
+    console.log(numero += 1)
+}
