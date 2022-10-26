9699a607b07eacc9aab331d6f16b9f1031ce62b6
GIGIPR3 <gigiprencipe47@gmail.com>
2022-10-26 10:59:42 +0200

    completamento

--- snack3/main.js
+++ snack3/main.js
@@ -3,5 +3,15 @@
 ! Snack 3 
 Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari */
 
-const numeri = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+let numeri = [3,7,9,12,220,3233,33,17];
 
+let sum = 0;
+
+for (let i=0; i < numeri.length; i++){
+
+    if (i % 2 != 0){
+        sum = sum + numeri[i];
+    }
+}
+
+console.log(sum);
--- snack4/main.js
+++ snack4/main.js
@@ -1,3 +1,34 @@
 /* 
 ! Snack 4 (Bonus)
 Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro. */
+
+let array1 = [2,7,9,6,7,8,1221,3,3,44,5,2];
+let array2 = [11];
+
+let arrayMaggiore;
+let arrayMinore;
+
+
+
+if ( array1.length > array2.length ){
+
+    arrayMaggiore = array1;
+    arrayMinore = array2;
+    console.log(arrayMaggiore);
+} else if (array1.length == array2.length){
+    console.log('gli array hanno lo stesso numero di dati');
+}else {
+    arrayMaggiore = array2;
+    arrayMinore = array1;
+}
+
+let differenzaLunghezzaArray = arrayMaggiore.length - arrayMinore.length;
+
+for (let i = 0; i < differenzaLunghezzaArray; i++){
+    let numeroRandom = Math.floor (Math.random() * 100);
+    console.log (`numero ciclo: ${i} e il numero random è: ${numeroRandom}`)
+
+    arrayMinore.push( numeroRandom );
+}
+
+console.log(arrayMinore);
